09464656a8f10a6d5a1fe538a98f215b6a7ec5b8
John Elto <John Elto@JohnElto-PC>
2016-10-25 20:59:10 -0300

    primeiro commit

---
+++ TAF_Petshop/src/main/java/com/TAF/TafPetshopApplication.java
@@ -0,0 +1,12 @@
+package com.TAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TafPetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TafPetshopApplication.class, args);
+	}
+}
---
+++ TAF_Petshop/src/test/java/com/TAF/TafPetshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.TAF;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TafPetshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
